7af51c47c8b371d366b867107490e3877c67a6d3
up1 <somkiat.p@gmail.com>
2024-06-01 13:14:39 +0700

    Update

--- workshop/java/src/main/java/com/example/demo/OpenTelemetryConfig.java
+++ workshop/java/src/main/java/com/example/demo/OpenTelemetryConfig.java
@@ -1,11 +1,16 @@
 package com.example.demo;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Metrics;
 import io.opentelemetry.api.common.AttributeKey;
 import io.opentelemetry.api.trace.SpanKind;
 import io.opentelemetry.contrib.sampler.RuleBasedRoutingSampler;
 import io.opentelemetry.sdk.autoconfigure.spi.AutoConfigurationCustomizerProvider;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Optional;
 
 @Configuration
 public class OpenTelemetryConfig {
@@ -18,5 +23,15 @@ public class OpenTelemetryConfig {
                                 RuleBasedRoutingSampler.builder(SpanKind.SERVER, fallback)
                                         .drop(AttributeKey.stringKey("url.path"), "^/actuator")
                                         .build());
+    }
+
+    @Bean
+    @ConditionalOnClass(name = "io.opentelemetry.javaagent.OpenTelemetryAgent")
+    public MeterRegistry otelRegistry() {
+        Optional<MeterRegistry> otelRegistry = Metrics.globalRegistry.getRegistries().stream()
+                .filter(r -> r.getClass().getName().contains("OpenTelemetryMeterRegistry"))
+                .findAny();
+        otelRegistry.ifPresent(Metrics.globalRegistry::remove);
+        return otelRegistry.orElse(null);
     }
 }
--- workshop/java/src/main/java/com/example/demo/kafka/KafkaProducerConfig.java
+++ workshop/java/src/main/java/com/example/demo/kafka/KafkaProducerConfig.java
@@ -3,6 +3,7 @@ package com.example.demo.kafka;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
@@ -10,11 +11,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaAdmin;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 
 @Configuration
 public class KafkaProducerConfig {
+
+    @Bean
+    public KafkaAdmin admin() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:29092");
+        return new KafkaAdmin(configs);
+    }
 
     @Bean
     public ProducerFactory<String, String> producerFactory() {
